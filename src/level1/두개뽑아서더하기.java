c284208a15ad272faf1447a5616dc1dd75bf8a10
Jun <rardin@naver.com>
2020-09-17 19:54:49 +0900

    0917_프로그래머스 - 두개뽑아서더하기

--- src/level1/두개뽑아서더하기.java
+++ src/level1/두개뽑아서더하기.java
@@ -2,40 +2,49 @@ package level1;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
-import java.util.TreeSet;
+import java.util.Set;
 
 public class 두개뽑아서더하기 {
 	public static void main(String[] args) {
 		int[] numbers = {2,1,3,4,1};
 		int[] answer = {};
-		//Object list = new Object();
-		HashSet<Integer> hashset = new HashSet<Integer>();
 		
-		//TreeSet<Integer> treeset = new TreeSet<Integer>();
-		
-		//List<Integer> list = new ArrayList<Integer>();
+		Set<Integer> set = new HashSet();
 		
 		for(int i=0; i<numbers.length-1; i++) {
 			for(int j=i+1; j<numbers.length; j++) {
-				//list.add(numbers[i] + numbers[j]);
-				hashset.add(numbers[i] + numbers[j]);
-				//treeset.add(numbers[i] + numbers[j]);				
+				//arrayList.add(numbers[i]+numbers[j]);
+				set.add(numbers[i]+numbers[j]);
 			}
 		}
 		
-		answer =  hashset.stream().mapToInt(i->i).toArray();
+		List<Integer> arrayList = new ArrayList<Integer>(set);
 		
-//		String result[] = hashset.toArray(new String[hashset.size()]);
-//		
-//		for(int i=0; i<result.length; i++) {
-//			answer[i] = Integer.parseInt(result[i]);
+		Collections.sort(arrayList);
+		int size = arrayList.size();
+		for(int i=0; i<size-1; i++) {
+			if(arrayList.get(i) == arrayList.get(i+1))
+				arrayList.remove(i+1);
+		}
+		
+//		HashSet<Integer> hashset = new HashSet<Integer>();
+//
+//		for(int i=0; i<numbers.length-1; i++) {
+//			for(int j=i+1; j<numbers.length; j++) {
+//				hashset.add(numbers[i] + numbers[j]);
+//			}
 //		}
-		//System.out.println("hashset : " + hashset);
-		//System.out.println("treeset : " + treeset);
-		//System.out.println("list : " + list);
+//		
+//		answer =  hashset.stream().mapToInt(i->i).toArray();
+		answer = new int[arrayList.size()];
+		for(int i=0; i<arrayList.size(); i++) {
+			answer[i] = arrayList.get(i);
+		}
+		
+		System.out.println("arrayList : " + arrayList.get(0));
 		System.out.println("answer : " + answer);
 	}
 }
