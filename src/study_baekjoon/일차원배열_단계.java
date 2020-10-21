2d8375a7c65b26e8f372cf6405cc19a96ee25d27
Jun <rardin@naver.com>
2020-10-21 23:23:34 +0900

    1021_2577번

--- src/study_baekjoon/일차원배열_단계.java
+++ src/study_baekjoon/일차원배열_단계.java
@@ -3,27 +3,51 @@ package study_baekjoon;
 import java.util.Scanner;
 
 public class 일차원배열_단계 {
-	
-	//Q2562 최댓값
+	//Q2577 숫자의 개수
 	public static void main(String[] args) {
-		int[] list = new int[9];
-		
 		Scanner in = new Scanner(System.in);
-		int max = 0;
-		for(int i=0; i<9; i++) {
-			list[i] = in.nextInt();
-			if(list[i] > max)
-				max = list[i];
-		}
 		
-		for(int i=0; i<9; i++) {
-			if(list[i] == max) {
-				System.out.println(list[i]);
-				System.out.println(i+1);
-				break;
+		int A = in.nextInt();
+		int B = in.nextInt();
+		int C = in.nextInt();
+		
+		int[] answer = new int[9];
+		
+		String ABC = Integer.toString(A*B*C);
+		//System.out.println(ABC);
+		
+		for(int i=1; i<10; i++) {
+			for(int j=0; j<ABC.length(); j++) {
+				if(i == Integer.parseInt(ABC.substring(j, j+1)) )
+					answer[i-1]++;
 			}
 		}
+		
+		for(int i=0; i<answer.length; i++)
+			System.out.println(answer[i]);
 	}
+	
+	
+	//Q2562 최댓값
+//	public static void main(String[] args) {
+//		int[] list = new int[9];
+//		
+//		Scanner in = new Scanner(System.in);
+//		int max = 0;
+//		for(int i=0; i<9; i++) {
+//			list[i] = in.nextInt();
+//			if(list[i] > max)
+//				max = list[i];
+//		}
+//		
+//		for(int i=0; i<9; i++) {
+//			if(list[i] == max) {
+//				System.out.println(list[i]);
+//				System.out.println(i+1);
+//				break;
+//			}
+//		}
+//	}
 	
 	
 	//Q10818번 최소, 최대
