cdc3f652e83ee3ec43c8c68a269c94daa76d4f08
Jun <rardin@naver.com>
2020-10-30 19:40:21 +0900

    1030_스터디

--- src/study_baekjoon/SQ11404_플로이드.java
+++ src/study_baekjoon/SQ11404_플로이드.java
@@ -3,57 +3,108 @@ package study_baekjoon;
 import java.util.Scanner;
 
 public class SQ11404_플로이드 {
-
+	static int N, M;
+	
+	static int[][] maps;
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
+		N = in.nextInt();
+		M = in.nextInt();
 		
-		//첫째 줄 : 도시의 갯수 n
-		int city = in.nextInt();
+		maps = new int[N+1][N+1];
 		
-		//둘째 줄 : 버스의 갯수 m
-		int bus = in.nextInt();
-		
-		//샛째 줄 ~ m+2줄 : 버스의 정보 (출발, 도착, 비용)
-		int[][] busInfo = new int[bus][3];
-		
-		for(int i=0; i<bus; i++) {
-			busInfo[i][0] = in.nextInt();
-			busInfo[i][1] = in.nextInt();
-			busInfo[i][2] = in.nextInt();			
+		for(int i=0; i<M; i++) {
+			int start = in.nextInt();
+			int end = in.nextInt();
+			int price = in.nextInt();
+			
+			//최솟값 입력
+			if(maps[start][end] == 0)
+				maps[start][end] = price;
+			else
+				maps[start][end] = Math.min(maps[start][end], price);
 		}
 		
-		//출력 : n개의 줄, i, j 도시 i에서 j로 가는데 필요한 최소 비용
-		//i에서 j로 갈 수 없는 경우 0 출력
-		int[][] cost = new int[city][city];
-	
-		for(int i=0; i<city; i++) {
-			int start = busInfo[i][0];
-			int end = busInfo[i][1];
-			int price = busInfo[i][2];
-			for(int j=0; j<city; j++) {
-				//시작과 끝 같은건 0 처리
-				if(i==j)
-					cost[i][j] = 0;
-				//경로가 일치하고, 비용이 더 작을 때 덮어쓰기
-				if(j==end && cost[i][j] > price)
-					cost[i][j] = price;
-			}
-		}
-		//플로이드 : 중간 경유지 거쳤을 때?
-		for(int start=0; start<city; start++) {
-			for(int mid=0; mid<city; mid++) {
-				for(int end=0; end<city; end++){
+		in.close();
+		
+		//플로이드 와샬? 워셜?
+		for(int mid=1; mid<=N; mid++) {
+			for(int start=1; start<=N; start++) {
+				if(maps[start][mid] == 0)
+					continue;
+				
+				for(int end=1; end<=N; end++) {
+					if(maps[mid][end] == 0 || start == end)
+						continue;
 					
+					if(maps[start][end] == 0 || maps[start][end] > maps[start][mid] + maps[mid][end]) {
+						maps[start][end] = maps[start][mid] + maps[mid][end];
+					}
 				}
 			}
 		}
 		
-		//출력
-		for(int i=0; i<city; i++) {
-			for(int j=0; j<city; j++) {
-				System.out.println(cost[i][j]);
+		for(int x=1; x<=N; x++) {
+			for(int y=1; y<=N; y++) {
+				//System.out.println(maps[x][y]);
+				System.out.printf("%d ", maps[x][y]);
 			}
+			System.out.println();
 		}
 	}
+	
+	
+	//입력 배열부터 만든 것이 꼬이게 만들었다.
+//	public static void main(String[] args) {
+//		Scanner in = new Scanner(System.in);
+//		
+//		//첫째 줄 : 도시의 갯수 n
+//		int city = in.nextInt();
+//		
+//		//둘째 줄 : 버스의 갯수 m
+//		int bus = in.nextInt();
+//		
+//		//샛째 줄 ~ m+2줄 : 버스의 정보 (출발, 도착, 비용)
+//		int[][] busInfo = new int[bus][3];
+//		
+//		for(int i=0; i<bus; i++) {
+//			busInfo[i][0] = in.nextInt();
+//			busInfo[i][1] = in.nextInt();
+//			busInfo[i][2] = in.nextInt();			
+//		}
+//		
+//		//출력 : n개의 줄, i, j 도시 i에서 j로 가는데 필요한 최소 비용
+//		//i에서 j로 갈 수 없는 경우 0 출력
+//		int[][] cost = new int[city][city];
+//	
+//		for(int i=0; i<city; i++) {
+//			int start = busInfo[i][0];
+//			int end = busInfo[i][1];
+//			int price = busInfo[i][2];
+//			for(int j=0; j<city; j++) {
+//				//시작과 끝 같은건 0 처리
+//				if(i==j)
+//					cost[i][j] = 0;
+//				//경로가 일치하고, 비용이 더 작을 때 덮어쓰기
+//				if(j==end && cost[i][j] > price)
+//					cost[i][j] = price;
+//			}
+//		}
+//		//플로이드 : 중간 경유지 거쳤을 때?
+//		for(int start=0; start<city; start++) {
+//			for(int mid=0; mid<city; mid++) {
+//				for(int end=0; end<city; end++){
+//					
+//				}
+//			}
+//		}
+//		
+//		//출력
+//		for(int i=0; i<city; i++) {
+//			for(int j=0; j<city; j++) {
+//				System.out.println(cost[i][j]);
+//			}
+//		}
+//	}
 
 }
--- src/study_baekjoon/SQ11727_2xn타일링2.java
+++ src/study_baekjoon/SQ11727_2xn타일링2.java
@@ -3,27 +3,56 @@ package study_baekjoon;
 import java.util.Scanner;
 
 public class SQ11727_2xnÅ¸ÀÏ¸µ2 {
+	
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
 		
-		int answer = 1;
-		int temp = 2;
+		int N = in.nextInt();
 		
-		while(n > 0) {
-			answer += (pactorial(n)*2)/(pactorial(n-temp)*(temp/2));
-			temp += 2;
-			n -= 2;
+		if(N==1) {
+			System.out.println(1);
 		}
-	
-		System.out.println(answer%10007);
+		
+		if(N==2) {
+			System.out.println(2);
+		}
+		
+		int[] memo = new int[N+1];
+		memo[1] = 1;
+		memo[2] = 3;
+		
+		for(int i=3; i<=N; i++) {
+			int tile1 = memo[i-1];
+			int tile2 = memo[i-2];
+			int tile3 = memo[i-2];
+			
+			memo[i] = tile1 + tile2 + tile3;
+		}
+		
+		System.out.println(memo[N]%10007);
 	}
 	
-	public static int pactorial(int n) {
-		int result = 1;
-		for(int i=n; i>0; i--) {
-			result *= i;
-		}
-		return result;
-	}
+//	public static void main(String[] args) {
+//		Scanner in = new Scanner(System.in);
+//		int n = in.nextInt();
+//		
+//		int answer = 1;
+//		int temp = 2;
+//		
+//		while(n > 0) {
+//			answer += (pactorial(n)*2)/(pactorial(n-temp)*(temp/2));
+//			temp += 2;
+//			n -= 2;
+//		}
+//	
+//		System.out.println(answer%10007);
+//	}
+//	
+//	public static int pactorial(int n) {
+//		int result = 1;
+//		for(int i=n; i>0; i--) {
+//			result *= i;
+//		}
+//		return result;
+//	}
 }
