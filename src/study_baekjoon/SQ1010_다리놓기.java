15ae88f32dab94b8bdaeffb225f3ba853e9ea853
Jun <rardin@naver.com>
2020-11-05 19:45:43 +0900

    1105_스터디

--- src/study_baekjoon/SQ1010_다리놓기.java
+++ src/study_baekjoon/SQ1010_다리놓기.java
@@ -1,10 +1,37 @@
 package study_baekjoon;
 
+import java.util.Scanner;
+
 public class SQ1010_다리놓기 {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		//int로 하니 숫자가 커지면 답이 0이되서 나온다.
+		double[] answer = new double[T];
 		
-
+		for(int i=0; i<T; i++) {
+			int N = in.nextInt();
+			int M = in.nextInt();
+		
+			if(N == M)
+				answer[i] = 1;
+			else{
+				answer[i] = factorial(M)/(factorial(M-N)*factorial(N));
+			}
+		}
+		
+		for(int i=0; i<T; i++) {
+			System.out.printf("%.0f%n",answer[i]);
+		}
 	}
 
+	public static double factorial(double n) {
+		double result = 1;
+		while(n>1) {
+			result *= n;
+			n--;
+		}
+		return result;
+	}
 }
