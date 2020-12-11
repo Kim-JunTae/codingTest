b744bc8dcc0752b42a1cd52c3b7171c57464eb30
Jun <rardin@naver.com>
2020-12-11 22:54:11 +0900

    [2020.12.11] 다시.

--- src/study_baekjoon/수학1_단계.java
+++ src/study_baekjoon/수학1_단계.java
@@ -10,32 +10,8 @@ public class 수학1_단계 {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-		String request = br.readLine();		
-		int N = Integer.parseInt(request);
-		
-		int count5kg = 0;	//5kg 봉지 수
-		int count3kg = 0;	//3kg 봉지 수
-		
-		//최소 봉지 갯수
-		int minCount;
-		
-		//최대한 5kg 봉지 수를 확보한다.
-		count5kg = N/5;
-		N = N-count5kg*5;
-		
-		while(N%3 != 0) {
-			if(count5kg < 0) {
-				System.out.println(-1);
-				break;
-			}
-			count5kg -= 1;
-			count3kg += 1;
-			N += 5;
-			
-			if(N%3 == 0) {
-				System.out.println(count5kg+count3kg);
-			}
-		}
+		int N = Integer.parseInt(br.readLine());
+		// 다시 시작
 	}	
 	
 	//손익 분기점
